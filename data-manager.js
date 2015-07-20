e085d8dc4531c10d638360785d7c207a9186bcc6
aluizio <aluizio@dcc.ufrj.br>
2015-07-20 09:49:43 -0300

    fixed a little bug on reading.

--- data-manager.js
+++ data-manager.js
@@ -19,44 +19,15 @@ function DataManager(gm){  //recebe como parâmetro uma instância da classe Gra
 		var c = [];
 		for(var i=0; i<gm.numberOfVariables; i++){
 			var $id = $(variableOnFunction_id+(i+1));
+
 			var value = parseFloat($id.val());
+			if(isNaN(value)){
+				c.push(1);
+				continue;
+			}
 			c.push(value);
 		}
 		return c;
-	};
-
-	this.getVectorSign = function(){
-		var signs = [];
-		for(var i=0; i<gm.numberOfConstraints; i++){
-			var $id = $(sign_id+(i+1));
-			var value = $id.val();
-			signs.push(value);
-		}
-		return signs;
-	};
-
-	this.getVectorB = function(){
-		var b = [];
-		for(var i=0; i<gm.numberOfConstraints; i++){
-			var $id = $(b_id+(i+1));
-			var value = parseFloat($id.val());
-			b.push(value);
-		}
-		return b;
-	};
-
-	this.getConstraintsMatrix = function(){
-		var matrix = []; // lista de listas;
-		for(var i=0; i<gm.numberOfConstraints; i++){
-			var list = [];
-			for(var j=0; j<gm.numberOfVariables; j++){
-				var $id = $(variable_id+(i+1)+"_"+(j+1));
-				var value = parseFloat($id.val());
-				list.push(value);
-			}
-			matrix.push(list);
-		}
-		return matrix;
 	};
 
 	this.getLPP = function(){
@@ -70,6 +41,10 @@ function DataManager(gm){  //recebe como parâmetro uma instância da classe Gra
 			for(var j=0; j<gm.numberOfVariables; j++){
 				var $id = $(variable_id+(i+1)+"_"+(j+1));
 				var value = parseFloat($id.val());
+				if(isNaN(value)){
+					list.push(1);
+					continue;
+				}
 				list.push(value);
 			}
 
@@ -78,6 +53,9 @@ function DataManager(gm){  //recebe como parâmetro uma instância da classe Gra
 
 			var $Bid = $(b_id+(i+1));
 			var b_value = parseFloat($Bid.val());
+			if(isNaN(b_value)){
+				b_value = 1;
+			}
 
 			lpp.createConstraint(list, s_value, b_value);
 		}
