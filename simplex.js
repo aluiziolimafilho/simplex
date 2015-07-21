8e8732559d1031a1da250701430e9eec54109536
aluizio <aluizio@dcc.ufrj.br>
2015-07-21 13:10:30 -0300

    Added a function to generate the first part of simplex table.

--- simplex.js
+++ simplex.js
@@ -76,7 +76,62 @@ function SimplexTable(){
 
 	var that = this;
 
+	// função responsável por transforma a restrições de forma adequada para o simplex tabular.
+	var getConstraintTable = function(lpp){
+		var listOfSlack = [];
+		var numberOfSlacks = 0;
+		var jump1 = 0;
+		for(var i=0; i<lpp.getNumberOfLines(); i++){
+			var constraint = lpp.getConstraint(i);
+			switch(constraint.sign){
+				case '<':
+					numberOfSlacks++;
+					listOfSlack.push(1);
+					that.slackVariables.push(lpp.getNumberOfLines()+i-jump1);
+					break;
+				case '>':
+					numberOfSlacks++;
+					listOfSlack.push(-1);
+					that.slackVariables.push(lpp.getNumberOfLines()+i-jump1);
+					break;
+				case '=':
+					listOfSlack.push(0);
+					jump1++;
+					break;
+			}
+		}
+		
+		var tempTable = [];
+		var jump2 = 0;
+
+		for(var i=0; i<lpp.getNumberOfLines(); i++){
+			
+			var slacks = math.zeros(numberOfSlacks);
+			if(listOfSlack[i] == 0) jump2++;
+			else{
+				slacks.subset(math.index(i-jump2), listOfSlack[i]);
+			}
+
+			var constraint = lpp.getConstraint(i);
+			
+			var line = math.matrix(constraint.values);
+			line = math.concat(line,slacks);
+
+			tempTable.push(line);
+		}
+		tempTable = math.matrix(tempTable);
+		return tempTable;
+
+	};
+
+	// função reponsável por gerar os custos para o simplex tabular.
+	var getCostLine = function(lpp){
+		
+	};
+
 	this.transformFromLPPToSimplexTable = function(lpp){
+		var constraintTable = getConstraintTable(lpp);
+		var costLine = getCostLine(lpp);
 	};
 }
 
